01186636f81000eb252bf07548cd8492b6383ac1
Ivo Draganov <idraganov91@gmail.com>
2025-11-12 21:24:52 +0200

    Update home.js

--- src/home.js
+++ src/home.js
@@ -12,6 +12,8 @@ export const Home = () => html`
                     <img src="./images/email.png" class="social-icon"></a><a
                     href="https://www.instagram.com/ivo.draganov/" title="Instagram">
                     <img src="./images/instagram.png" class="social-icon"></a>
+                    <a href="https://www.buymeacoffee.com/draganov.ivo" title="Buy me a coffe">
+                    <img src="./images/bff.svg" class="social-icon"></a>
                 </div>
             </div>
         </div>
