623da774bd2831da67956ec47829e83bba45a35c
Ivo Draganov <idraganov91@gmail.com>
2026-01-26 13:01:51 +0200

    Fix social icons and update email link

--- src/home.js
+++ src/home.js
@@ -8,13 +8,10 @@ export const Home = () => html`
                 <h1>Ivo Draganov</h1>
                 <p class="description">Proffesional Truck Driver</p>
                 <div class="social-icons">
+                    <a href="https://www.instagram.com/ivo.draganov/" title="Instagram">
+                    <img src="./images/instagram.png" class="social-icon"></a>
                     <a href="/contact" title="Email">
-                    <img src="./images/email.png" class="social-icon">
-                    </a><a href="https://www.instagram.com/ivo.draganov/" title="Instagram">
-                    <img src="./images/instagram.png" class="social-icon">
-                    </a><a href="https://www.buymeacoffee.com/draganov.ivo" title="Buy me a coffee">
-                    <img src="./images/bff.svg" class="social-icon">
-                    </a>
+                    <img src="./images/email.png" class="social-icon"></a>
                 </div>
             </div>
         </div>
